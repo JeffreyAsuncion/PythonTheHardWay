7ac703429180c417e0e33f6e61b1a32afd52fed6
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-08-28 07:25:20 -0400

    ex16.py writing to a file

---
+++ ex6.py
@@ -0,0 +1,20 @@
+x = f"There are {10} types of people."
+binary = 'binary'
+do_not = "don't"
+y = "Those who know {binary} adn those who {do_not}."
+
+print(x)
+print(y)
+
+print('I said: {x}.')
+print('I also said: {y}')
+
+hilarious = False
+joke_evaluation = f"Isn't that joke so funny?! {hilarious}"
+
+print(joke_evaluation)
+
+w = "This is the left side of ..."
+e = "a string with a right side."
+
+print(w + e)
