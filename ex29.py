e1d5c8b0572699ab120b2d05d2cab20c47d2d8f0
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-10-30 01:00:51 -0400

    exercise 29

---
+++ ex29.py
@@ -0,0 +1,34 @@
+
+# Exercise 29 - What If
+
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats:
+    print("Too many cats! The World is doomed!")
+
+if people > cats:
+    print("Not many cats! The world is saved!")
+
+if people < dogs:
+    print("The world is drooled man")
+
+if people > dogs:
+    print("The world is dry!")
+
+
+dogs += 5
+
+if people >= dogs:
+    print("People are greater than or equal to dogs.")
+
+if people <= dogs:
+    print("People are less than or equal to dogs.")
+
+if people == dogs:
+    print("People are dogs.")
+
+
+
