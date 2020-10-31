97488d99c9feff8b7737fc6910d0db03ee406550
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-10-31 07:09:29 -0400

    Exercise 42. Is-A, Has-A, Objects and Classes

---
+++ 42.py
@@ -0,0 +1,82 @@
+# Exercise 42. Is-A, Has-A, Objects, and Classes
+
+## Animal is-a object (yes, sort of confusing) look at the extra credit
+
+class Animal(object):
+    pass
+
+
+## ?? Dog is-a Animal
+class Dog(Animal):
+
+    def __init__(self, name):
+        ## ?? Dog has-a name
+        self.name = name
+
+## ?? Cat is-a Animal
+class Cat(Animal):
+
+    def __init__(self, name):
+        ## ?? Cat has-a name
+        self.name = name
+
+## ?? 
+class Person(object):
+
+    def __init__(self, name):
+        ## ?? Person has-a name
+        self.name = name
+
+        ## Person has-a pet of some kind
+        self.pet = None
+
+## ?? Employee is-a Person
+class Employee(Person):
+
+    def __init__(self, name, salary):
+        ## ?? hmm what is this strange magic?
+        super (Employee, self).__init__(name)
+        ## ?? Employee has-a salary
+        self.salary = salary
+
+## ?? 
+class Fish(object):
+    pass
+
+
+## ?? Salmon is-a Fish
+class Salmon(Fish):
+    pass
+
+
+## ?? Halibut is-a Fish
+class Halibut(Fish):
+    pass
+
+
+## rover is-a Dog
+rover = Dog("Rover")
+
+## ?? satan is-a Cat
+satan = Cat("Satan")
+
+## ?? mary is-a Person
+mary = Person("Mary")
+
+## ?? mary has-a pet is-a satan is-a cat
+mary.pet = satan
+
+## ?? frank is-a Employee, has-a name "Frank", has-a salary 120000
+frank = Employee("Frank", 120000)
+
+## ?? frank has-a pet, rover is-a pet
+frank.pet = rover
+
+## ?? flipper is-a Fish
+flipper = Fish()
+
+## ?? crouse is-a Salmon
+crouse = Salmon()
+
+## ?? harry is-a Halibut()
+harry = Halibut()
